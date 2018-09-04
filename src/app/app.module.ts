b0984fa007e55fef0b51ce12d9f70e2922f41314
Balu Kharade <kharadeb999@gmail.com>
2018-09-04 13:54:06 +0530

    delet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ErrorComponent
+    
   ],
   imports: [
     BrowserModule
