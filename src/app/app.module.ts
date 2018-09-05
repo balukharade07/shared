a9862988dd93e8175776d01b7548ca6ecb048dad
Balu Kharade <kharadeb999@gmail.com>
2018-09-05 12:39:51 +0530

    recentadd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FeatureS1Module } from './feature-s1/feature-s1.module';
-import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,FeatureS1Module,SharedModule
+    BrowserModule,FeatureS1Module
   ],
   providers: [],
   bootstrap: [AppComponent]
