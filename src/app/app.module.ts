c614006725ac36975cc7fa7f39d951f83bb5dc68
Balu Kharade <kharadeb999@gmail.com>
2018-09-08 12:20:21 +0530

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MydirectiveDirective } from './mydirective.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MydirectiveDirective,
     
   ],
   imports: [
