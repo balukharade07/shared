2a310ce3163861f8ee46089bc6ed707cf98f66d9
Balu Kharade <kharadeb999@gmail.com>
2018-09-09 12:19:40 +0530

    add

--- src/app/mydirective.directive.ts
+++ src/app/mydirective.directive.ts
@@ -1,10 +1,17 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer } from '@angular/core';
 
 @Directive({
   selector: '[appMydirective]'
 })
 export class MydirectiveDirective {
 
-  constructor() { }
+  constructor(public el: ElementRef, public rend:Renderer) { }
+  
+  //@Input()  check : boolean;
+ 
+
+  ngOnInit(){
+    this.rend.setElementStyle(this.el.nativeElement,'background-color','#ccc');
+      }
 
 }
