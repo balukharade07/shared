68adf175f0de27a98bc0315a2fc5a6d5a7978d27
Balu Kharade <kharadeb999@gmail.com>
2018-09-29 12:39:31 +0530

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'New';
+
+  submitted = false;
+
+  login={
+    name : '',
+    password : ''
+  };
+  login1={
+    name : 'balu',
+    password : 'balu'
+  };
+   
+  onSubmit(){
+if(this.login.name === this.login1.name && this.login.password === this.login1.password)
+{
+  alert("Login sucefully");
+  this.submitted = true;
+}
+else{
+  alert("please Login");
+}
+  }
 }
