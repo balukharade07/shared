999f0c36f699a5dce514531ce29d0b2e732bad95
Balu Kharade <kharadeb999@gmail.com>
2018-10-01 16:53:31 +0530

    adeed

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -7,13 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteComponent  {
 
-  quotes=[ "abc"];
+  quotes=[ "Balasaheb"];
   name = "";
+  item = false;
+  bindEmail = false;
   add(){
-    this.quotes.push(this.name);
+    if(this.quotes.length>10){
+       this.item = true;
+    }else{
+     
+      this.quotes.push(this.name);
+     
+    }
+   
   }
-  remove(){
-    this.quotes.splice(-1);
+  remove(a){
+    this.item = false;
+    this.quotes.splice(a,1);
+  }
+
+  function(){
+   if( this.bindEmail == false)
+   {
+    this.bindEmail = true;
+   }else{
+    this.bindEmail = false;
+   }
   }
  
 }
