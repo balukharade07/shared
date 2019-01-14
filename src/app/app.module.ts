d9a994cb26570f567488aafc428c7a8309330696
Balu Kharade <kharadeb999@gmail.com>
2019-01-14 21:08:04 +0530

    push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PushComponent } from './push/push.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PushComponent,
     
   ],
   imports: [
